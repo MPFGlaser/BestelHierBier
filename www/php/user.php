2561a57ebe66aaf60900fb44b070dbdd5d344d15
Snobl,Jindra J.W <j.snobl@student.fontys.nl>
2020-09-17 15:48:08 +0200

    started working on user class

---
+++ www/php/user.php
@@ -0,0 +1,17 @@
+<?php
+    class User{
+        public $username;
+        public $email;
+        public $password;
+
+        function __construct($username, $email, $password){
+            $this->username = $username;
+            $this->email = $email;
+            $this->password = $password;
+        }
+
+        function get_name(){
+            return $this->username;
+        }
+    }
+?>
