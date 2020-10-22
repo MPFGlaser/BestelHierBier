a11edab1777365009cd4c4f630cab62b5a278cd1
Max Glaser <mpf.glaser@gmail.com>
2020-10-22 19:47:29 +0200

    Updated header to work within subfolders without complaining.

--- www/views/header.php
+++ www/views/header.php
@@ -1,6 +1,6 @@
 <div class="grid-container-top">
         <div class="grid-item">
-            <a href="/index.php"><img src="images/tempLogo.png" alt="Temp Logo" /></a>
+            <a href="/index.php"><img src="/images/tempLogo.png" alt="Temp Logo" /></a>
         </div>
         <div class="grid-item-top">
             <h1 id="title">Bestel Hier Bier</h1>
