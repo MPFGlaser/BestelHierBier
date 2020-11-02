ede9ca9f47602b33a18708f5779a3e5cb7b40670
Max Glaser <mpf.glaser@gmail.com>
2020-11-02 15:31:24 +0100

    Minor code adjustment to make it less ugly

--- www/js/RegisterMode.js
+++ www/js/RegisterMode.js
@@ -2,7 +2,7 @@ function registerMode() {
     var rf = document.getElementById("registerForm");
     var lf = document.getElementById("loginForm");
     var cb = document.getElementById("registerCheckbox");
-    if (cb.checked == true) {
+    if (cb.checked) {
         lf.style.display = 'none';
         rf.style.display = 'block';
     } else {
