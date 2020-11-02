a7b58dfea6511e251b83b50fb1d248aa7526628a
Snobl,Jindra J.W <j.snobl@student.fontys.nl>
2020-11-02 21:25:06 +0100

    Clicking the found product name now takes you to the correct product page

--- www/js/productSearch.js
+++ www/js/productSearch.js
@@ -11,11 +11,16 @@ function dynamicSearch(searchString){
         success: function(data){
             var beerArray = JSON.parse(data);
             for(var i = 0; i < beerArray.length; i++){
-                console.log(beerArray[i]["name"]);
+                var foundName = beerArray[i]["name"];
+                var foundBrewery = beerArray[i]["brewery"];
+                var foundCategory = beerArray[i]["category"];
+                var foundImgURL = beerArray[i]["imageURL"];
+                var foundId = beerArray[i]["id"];
+                var foundAbv = beerArray[i]["abv"];
 
-                var newWindowLocation = "/products/view.php?id=" + beerArray[i]["id"] ;
+                var newWindowLocation = "/products/view.php?id="+foundId;
 
-                foundItemsClass.innerHTML += '<div class="product"><div class="productImage"><a href="/product.php?id="'+beerArray[i]["id"]+'><img "src=/images/"'+beerArray[i]["imageURL"]+'"alt="'+beerArray[i]["name"]+' /> </a> </div> <div class="productDescription"> <a href="/product.php?id="'+beerArray[i]["id"]+'> <h1>'+beerArray[i]["name"]+' ('+beerArray[i]["abv"]+')</h1> </a><br> <p>'+beerArray[i]["category"]+' by '+beerArray[i]["brewery"]+'</p> </div> <div class="button"> <button onclick="window.location.href='+newWindowLocation+'">Learn more</button> </div> </div>'
+                foundItemsClass.innerHTML += '<div class="product"><div class="productImage"><a href="../product.php?id='+foundId+'"><img src"=../images/'+foundImgURL+'"alt="'+foundName+'"/> </a> </div> <div class="productDescription"> <a href="../product.php?id='+foundId+'"> <h1>'+foundName+' ('+foundAbv+')</h1> </a><br> <p>'+foundCategory+' by '+foundBrewery+'</p> </div> <div class="button"> <button onclick="window.location.href='+newWindowLocation+'">Learn more</button> </div> </div>'
             }
         }
     });
