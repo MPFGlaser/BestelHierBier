5e7e22a2139209ebce7749e1f55873b61d5f162e
Snobl,Jindra J.W <j.snobl@student.fontys.nl>
2021-01-01 20:45:09 +0100

    Added comments to celarify ajaxCallRegulator.php

--- www/php/Core/ajaxCallRegulator.php
+++ www/php/Core/ajaxCallRegulator.php
@@ -1,4 +1,10 @@
 <?php
+/*
+ * This class regulates all of the calls to php functions made by ajax. The way
+ * this is done is by the following switch statement, instead of ajax calling to
+ * a specific function it passes a functionId which works in the same way a
+ * function name would. It executes the code in this block as if it were a function.
+ */
 
 namespace Core;
 //For error viewing
@@ -16,16 +22,20 @@ use Core\PopulateFoundItems;
 
 switch ($_POST['functionId']) {
     case 1:
+        // Find beers by search string.
         $populate = new PopulateFoundItems();
         echo json_encode($populate->found(true, $_POST['searchString']));
         return;
         break;
     case 2:
+        // Find beers by selection of checkboxes.
         $populate = new PopulateFoundItems();
         echo json_encode($populate->foundByFilter(json_decode(stripslashes($_POST['checkedArray']))));
         return;
         break;
 
     default:
+        // In case someone accidentaly stumbles accros this file.
         echo "Not allowed";
+        break;
 }
